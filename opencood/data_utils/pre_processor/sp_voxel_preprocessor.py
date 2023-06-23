416f4a4c36d5f106386875027a704cc2520ae53d
Yunshuang Yuan <yunshuang.yuan@ikg.uni-hannover.de>
2023-06-23 12:46:48 +0200

    update

--- opencood/data_utils/pre_processor/sp_voxel_preprocessor.py
+++ opencood/data_utils/pre_processor/sp_voxel_preprocessor.py
@@ -11,6 +11,54 @@ from opencood.data_utils.pre_processor.base_preprocessor import \
     BasePreprocessor
 
 
+class VoxelGeneratorWrapper():
+    def __init__(self, vsize_xyz, coors_range_xyz, num_point_features, max_num_points_per_voxel, max_num_voxels):
+        try:
+            from spconv.utils import VoxelGeneratorV2 as VoxelGenerator
+            self.spconv_ver = 1
+        except:
+            try:
+                from spconv.utils import VoxelGenerator
+                self.spconv_ver = 1
+            except:
+                from spconv.utils import Point2VoxelCPU3d as VoxelGenerator
+                self.spconv_ver = 2
+
+        if self.spconv_ver == 1:
+            self._voxel_generator = VoxelGenerator(
+                voxel_size=vsize_xyz,
+                point_cloud_range=coors_range_xyz,
+                max_num_points=max_num_points_per_voxel,
+                max_voxels=max_num_voxels
+            )
+        else:
+            self._voxel_generator = VoxelGenerator(
+                vsize_xyz=vsize_xyz,
+                coors_range_xyz=coors_range_xyz,
+                num_point_features=num_point_features,
+                max_num_points_per_voxel=max_num_points_per_voxel,
+                max_num_voxels=max_num_voxels
+            )
+
+    def generate(self, points):
+        if self.spconv_ver == 1:
+            voxel_output = self._voxel_generator.generate(points)
+            if isinstance(voxel_output, dict):
+                voxels, coordinates, num_points = \
+                    voxel_output['voxels'], voxel_output['coordinates'], voxel_output['num_points_per_voxel']
+            else:
+                voxels, coordinates, num_points = voxel_output
+        else:
+            assert tv is not None, f"Unexpected error, library: 'cumm' wasn't imported properly."
+            voxel_output = self._voxel_generator.point_to_voxel(tv.from_numpy(points))
+            tv_voxels, tv_coordinates, tv_num_points = voxel_output
+            # make copy with numpy(), since numpy_view() will disappear as soon as the generator is deleted
+            voxels = tv_voxels.numpy()
+            coordinates = tv_coordinates.numpy()
+            num_points = tv_num_points.numpy()
+        return voxels, coordinates, num_points
+
+
 class SpVoxelPreprocessor(BasePreprocessor):
     def __init__(self, preprocess_params, train):
         super(SpVoxelPreprocessor, self).__init__(preprocess_params,
@@ -29,38 +77,17 @@ class SpVoxelPreprocessor(BasePreprocessor):
                      np.array(self.lidar_range[0:3])) / np.array(self.voxel_size)
         self.grid_size = np.round(grid_size).astype(np.int64)
 
-        try:
-            from spconv.utils import VoxelGeneratorV2 as VoxelGenerator
-            self.spconv_ver = 1
-        except:
-            try:
-                from spconv.utils import VoxelGenerator
-                self.spconv_ver = 1
-            except:
-                from spconv.utils import Point2VoxelCPU3d as VoxelGenerator
-                self.spconv_ver = 2
-        # use sparse conv library to generate voxel
-        if self.spconv_ver == 1:
-            self.voxel_generator = VoxelGenerator(
-                voxel_size=self.voxel_size,
-                point_cloud_range=self.lidar_range,
-                max_num_points=self.max_points_per_voxel,
-                max_voxels=self.max_voxels
-            )
-        else:
-
-            self.voxel_generator = VoxelGenerator(
-                vsize_xyz=self.voxel_size,
-                coors_range_xyz=self.lidar_range,
-                max_num_points_per_voxel=self.max_points_per_voxel,
-                num_point_features=4,
-                max_num_voxels=self.max_voxels
-            )
+        self.voxel_generator = VoxelGeneratorWrapper(
+            vsize_xyz=self.voxel_size,
+            coors_range_xyz=self.lidar_range,
+            max_num_points_per_voxel=self.max_points_per_voxel,
+            num_point_features=4,
+            max_num_voxels=self.max_voxels
+        )
 
     def preprocess(self, pcd_np):
         data_dict = {}
-        pcd_tv = tv.from_numpy(pcd_np)
-        voxel_output = self.voxel_generator.point_to_voxel(pcd_tv)
+        voxel_output = self.voxel_generator.generate(pcd_np)
         if isinstance(voxel_output, dict):
             voxels, coordinates, num_points = \
                 voxel_output['voxels'], voxel_output['coordinates'], \
