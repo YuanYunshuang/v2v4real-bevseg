6c9adcd534cc6009b66724193e71b60c43edd7a5
Yunshuang <yunshuang.yuan@@ikg.uni-hannover.de>
2023-07-03 21:57:46 +0200

    update

--- opencood/tools/exps.py
+++ opencood/tools/exps.py
@@ -3,8 +3,9 @@ from opencood.tools.inference import main
 
 if __name__=="__main__":
     exps = np.zeros((12, 2))
-    exps[:5, 0] = np.arange(0, 6) * 0.1
-    exps[5:, 1] = np.arange(0, 6) * 0.1
+    n = 6
+    exps[:n, 0] = np.arange(0, 6) * 0.1
+    exps[n:, 1] = np.arange(0, 6) * 0.1
 
     for exp in exps:
         main(exp)
